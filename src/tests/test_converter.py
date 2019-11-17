19bec5e10f337a7d95d6e91a40340da0bd488bfc
Evgenia Martynova <evg.martynova@gmail.com>
2019-11-17 17:01:40 +0100

    Clean up the code to convert OPD to TREC format, add tests

---
+++ src/tests/test_converter.py
@@ -0,0 +1,62 @@
+from unittest import TestCase
+import trec_converter as tc
+import os, shutil
+
+class TestConverter(TestCase):
+    def setUp(self):
+        for the_file in os.listdir("trec"):
+            file_path = os.path.join("trec", the_file)
+            try:
+                os.unlink(file_path)
+            except Exception as e:
+                print(e)
+
+    def test_build__trec_file_path(self):
+        output_path = "output/data/trec"
+        directory = "input/data/texts/1"
+        trec_path = tc.build__trec_file_path(output_path, directory)
+        self.assertEqual(trec_path, "output/data/trec/1.trectext")
+
+    def test_build__docno(self):
+        directory = "input/data/texts/7"
+        file_name = "5.txt"
+        docno = tc.build__docno(directory, file_name)
+        self.assertEqual(docno, "7_5")
+
+    def test_convert_text_to_trec_doc(self):
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " \
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " \
+               "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " \
+               "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        document_number = "6_99"
+
+        trec_doc = tc.convert_text_to_trec_doc(text, document_number)
+        trec_doc_expected = "<DOC>\n" \
+                   "<DOCNO>6_99</DOCNO>\n" \
+                   "<TEXT>\n" \
+                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " \
+                   "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " \
+                   "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " \
+                   "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n" \
+                   "</TEXT>\n" \
+                   "</DOC>\n\n"
+
+        self.assertEqual(trec_doc, trec_doc_expected)
+
+    def test_conversion(self):
+        tc.convert("opd", "trec")
+
+        trec_files = os.listdir("trec")
+        self.assertEqual(len(trec_files), 3)
+
+        for file in trec_files:
+            self.assertIn(file, ["1.trectext", "3.trectext", "5.trectext"])
+
+        # compare files content
+        for file_name in ["1.trectext", "3.trectext", "5.trectext"]:
+            generated_file = open("trec/" + file_name, mode='r')
+            expected_file = open("trec_expected/" + file_name, mode='r')
+            self.assertEqual(generated_file.read().strip(), expected_file.read().strip())
+            generated_file.close()
+            expected_file.close()
+
--- src/trec_converter.py
+++ src/trec_converter.py
@@ -12,38 +12,53 @@ text_end = "</TEXT>"
 
 new_line = "\n"
 
+trec_file_extension = ".trectext"
+
 # TODO: Add tests, clean up
+
+
+def build__trec_file_path(output_path, directory):
+    return output_path + "/" + os.path.basename(directory) + trec_file_extension
+
+
+def build__docno(directory, file_name):
+    return os.path.basename(directory) + "_" + os.path.splitext(file_name)[0]
+
+
+def read_file(directory, file_name):
+    file_path = directory + "/" + file_name
+    file = open(file_path, mode='r')
+    file_text = file.read().strip()
+    file.close()
+    return file_text
+
+
+def convert_text_to_trec_doc(text, document_number):
+    trec_doc = doc_begin + new_line
+    trec_doc += docno_begin + document_number + docno_end + new_line
+    trec_doc += text_begin + new_line + text + new_line + text_end + new_line
+    trec_doc += doc_end + new_line + new_line
+
+    return trec_doc
+
+
+def convert(opd_path, trec_path):
+    for directory, directories, files in os.walk(opd_path):
+        has_files = len(files) > 0
+        if has_files:
+            trec_file = open(build__trec_file_path(trec_path, directory), "w+")
+        for file_name in [x for x in files if x.endswith('.txt')]:
+            file_text = read_file(directory, file_name)
+            docno = build__docno(directory, file_name)
+            trec_file.write(convert_text_to_trec_doc(file_text, docno))
+
+        if has_files:
+            trec_file.close()
 
 def main():
     opd_data_path = "../../data/opd/texts"
-    output_data_path = "../../data/trec"
-
-    for directory, directories, files in os.walk(opd_data_path):
-        if len(files) > 0:
-            trec_file_path = output_data_path + "/" + os.path.basename(directory) + ".txt"
-            trec_file = open(trec_file_path, "w+")
-        for file_name in [x for x in files if x.endswith('.txt')]:
-            current_document = doc_begin + new_line
-            current_document_number = os.path.basename(directory) + "_" + os.path.splitext(file_name)[0]
-            current_document += docno_begin + current_document_number + docno_end + new_line
-            current_document += text_begin + new_line
-
-            file_path = directory + "/" + file_name
-            file = open(file_path, mode='r')
-            file_text = file.read()
-            file.close()
-
-            current_document += file_text
-            current_document += text_end + new_line + doc_end + new_line + new_line
-
-            trec_file.write(current_document)
-
-        if len(files) > 0:
-            trec_file.close()
-
-
-
-
+    output_path = "../../data/trec"
+    convert(opd_data_path, output_path)
 
 if __name__ == '__main__':
     main()
