a697e39b66bf6a8898fe77e23b8dfcb0dd14d802
Shima-yr <56838722+Shima-yr@users.noreply.github.com>
2019-11-26 17:50:14 +0100

    Update CosineSimilarity.py
    I changed it in a way of Affinity Graph

--- src/re_ranking/CosineSimilarity.py
+++ src/re_ranking/CosineSimilarity.py
@@ -1,22 +1,24 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import math
+
+
+def cos_similarity(vector1, vector2):
+    dot_product = sum(p*q for p,q in zip(vector1, vector2))
+    magnitude = math.sqrt(sum([val**2 for val in vector1]))
+    if not magnitude:
+        return 0
+    return dot_product/magnitude
 
 
 def getSimilarityMatrix(docs):
-    docs_name = []
-    # doc_id = 0
-    for i in range(1,len(docs)+1):
-        docs_name.append("DOC %d" % i )
+    tfidf_vectorizer = TfidfVectorizer()
+    vectorized_matrix = tfidf_vectorizer.fit_transform(docs)
+    result_matrix = []
+    for count_0, doc_0 in enumerate(vectorized_matrix.toarray()):
+        for count_1, doc_1 in enumerate(vectorized_matrix.toarray()):
+            result_matrix.append((cos_similarity(doc_0, doc_1), count_0, count_1))
+    return result_matrix
 
-    # vectorizer=CountVectorizer(stop_words='english')
-    # vectorizer=CountVectorizer()
-    vectorizer = TfidfVectorizer()
-    sparse_matrix = vectorizer.fit_transform(docs)
-    doc_term_matrix = sparse_matrix.todense()
-
-    df = pd.DataFrame(doc_term_matrix, columns=vectorizer.get_feature_names(), index=docs_name)
-    return cosine_similarity(df, df)
 
 
 def main():
@@ -24,9 +26,10 @@ def main():
     doc_election = "President Trump says Putin had no political interference is the election outcome. He says it was a witchhunt by political parties. He claimed President Putin is a friend who had nothing to do with the election"
     doc_putin = "Post elections, Vladimir Putin became President of Russia. President Putin had served as the Prime Minister earlier in his political career"
     documents = [doc_trump, doc_election, doc_putin]
-    result = getSimilarityMatrix(documents)
+    result=getSimilarityMatrix(documents)
     print(result)
 
 
 if __name__ == '__main__':
     main()
+
