3a74f3bc67d07980ddcc60c6aec2309e49358288
Evgenia Martynova <evg.martynova@gmail.com>
2019-12-07 16:41:38 +0100

    Code clean up

--- src/data_collection/wiki_data.py
+++ src/data_collection/wiki_data.py
@@ -64,19 +64,49 @@ def extract_disambiguation_pages(url):
         links = subcategory_html.find_all('a')
         for link in links:
             dis_pages.append((link.get("href"), link.get("title")))
-    print(dis_pages)
+    print("Extracted disambiguation pages %s" % dis_pages)
     return dis_pages
 
 
-def extract_topics_aspects(disambiguation_page_title):
+def extract_topics_with_aspects(disambiguation_pages):
+    pages = {}
+    for _, title in disambiguation_pages:
+        print("Extracting aspects of %s" % title)
+        aspects = extract_topic_aspects(title)
+        if aspects is not None:
+            # remove (disambiguation) from title to keep documents collection uncluttered
+            topic = re.sub('\(disambiguation\)$', '', title)
+            pages[topic] = aspects
+
+    return pages
+
+
+def extract_topic_aspects(disambiguation_page_title):
     try:
-        page = wikipedia.page(disambiguation_page_title, auto_suggest=False)
+        # we try to access disambiguation page to raise wikipedia.exceptions.DisambiguationError
+        _ = wikipedia.page(disambiguation_page_title, auto_suggest=False)
         return None
     except wikipedia.exceptions.DisambiguationError as disambiguation:
+        # disambiguation.options is an array of titles of all wikipedia pages to which a term may refer.
+        # We filter them to exclude those which for sure lead to another disambiguation pages
         aspects = [title for title in disambiguation.options if "(disambiguation)" not in title]
         return aspects
     except:
         print("Failed to resolve ambiguity %s" % disambiguation_page_title)
+        return None
+
+
+def extract_aspect(aspect_title):
+    try:
+        return wikipedia.page(aspect_title, auto_suggest=False)
+    except wikipedia.exceptions.DisambiguationError:
+        print("Page Error %s" % aspect_title)
+        return None
+    except wikipedia.exceptions.PageError:
+        print("Page non existent %s" % aspect_title)
+        return None
+    except:
+        print("Unknown Error %s" % aspect_title)
         return None
 
 
@@ -102,51 +132,32 @@ def save_page_topic_aspects(topic, aspects):
         f.write(str(index) + "\t" + aspect + "\n")
     f.close()
 
+
 def download_wiki_data(links):
     create_topics_texts_directory()
     create_topics_aspects_directory()
     for page in links:
-        #url = dis_page.url + "?from=" + letter
+        # url = dis_page.url + "?from=" + letter
         url = wiki_url + page
-        disambiguation_pages = extract_disambiguation_pages(url)
+        disambiguation_pages = extract_disambiguation_pages(url)[0:5]
+        topics_with_aspects = extract_topics_with_aspects(disambiguation_pages)
+        topics = topics_with_aspects.keys()
 
-        # collect links of disambiguation pages
-        pages = dict()
-        for link, title in disambiguation_pages:
-            # remove (disambiguation) from pages that have it and the remaining whitespace
-            topic = re.sub('\(disambiguation\)$', '' ,title)
-            print(topic)
-            aspects = extract_topics_aspects(title)
-            if aspects is not None:
-                pages[topic] = aspects
-        print(pages)
-
-        topics = pages.keys()
         for topic in topics:
             topic_directory = create_topic_directory(topic)
             if topic_directory is not None:
-                aspect_pages = pages[topic]
-                aspects = []
-                for aspect_page in aspect_pages:
-                    if aspect_page:
-                        try:
-                            page = wikipedia.page(aspect_page, auto_suggest=False)
-                            page_id = uuid.uuid4()
-                            save_page(aspect_page, page.content, page_id, topic_directory)
-                            aspects.append((aspect_page, page_id))
-                        except wikipedia.exceptions.DisambiguationError:
-                            print("Page Error %s" % aspect_page)
-                            continue
-                        except wikipedia.exceptions.PageError:
-                            print("Page non existent %s" % aspect_page)
-                            continue
-                        except:
-                            print("Unknown Error %s" % aspect_page)
-                            continue
-                if len(aspects) == 0 or len(aspects) == 1:
+                aspect_pages = topics_with_aspects[topic]
+                extracted_aspects = []
+                for aspect_title in aspect_pages:
+                    aspect_page = extract_aspect(aspect_title)
+                    if aspect_page is not None:
+                        page_id = uuid.uuid4()
+                        save_page(aspect_title, aspect_page.content, page_id, topic_directory)
+                        extracted_aspects.append((aspect_title, page_id))
+                if len(extracted_aspects) == 0 or len(extracted_aspects) == 1:
                     shutil.rmtree(topic_directory)
                 else:
-                    save_page_topic_aspects(topic, aspects)
+                    save_page_topic_aspects(topic, extracted_aspects)
 
 
 
