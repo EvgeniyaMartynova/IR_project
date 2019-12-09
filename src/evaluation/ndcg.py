23b1929c8f77098b0342a9bd5d0fb6753423651a
Manuela <manuela.bergau@gmail.com>
2019-12-09 16:03:59 +0100

    precision, recall, f-score implementation

--- src/evaluation/ndcg.py
+++ src/evaluation/ndcg.py
@@ -9,7 +9,7 @@ def get_relevance(query):
     return [x.split("\t")[0] for x in relevant_docs]
 
 
-def evaluate(query, hits):
+def ndcg(query, hits):
     relevant_docs = get_relevance(query)
     # the first document is without discount
     if hits[0].docid in relevant_docs:
@@ -22,8 +22,29 @@ def evaluate(query, hits):
             ndcg += 1/(math.log2(i+1))
     return ndcg
 
+def precision_recall_f(query, hits):
+    # get top 10 results
+    hits = hits[:10]
+    # get all relevant docs for query
+    relevant_docs = get_relevance(query)
+    # get intersection of relevant docs and top 10 results
+    relevant_hits = [hit for hit in hits if hit.docid in relevant_docs]
+
+    precision = len(relevant_hits)/len(hits)
+    recall = len(relevant_hits)/len(relevant_docs)
+    f_score = 2*precision*recall/(precision+recall)
+
+    print("Precision: " + str(precision))
+    print("Recall: " + str(recall))
+    print("F-Score: " + str(f_score))
+
+
 def compare_results(query, hits_original, hits_reranked):
-    ndcg_original = evaluate(query,hits_original)
-    ndcg_reranked = evaluate(query,hits_reranked)
-    print("original: " + str(ndcg_original))
-    print("reranked: " + str(ndcg_reranked))
+    ndcg_original = ndcg(query,hits_original)
+    ndcg_reranked = ndcg(query,hits_reranked)
+    print("Original Ranking:")
+    print("ndcg: " + str(ndcg_original))
+    precision_recall_f(query, hits_original)
+    print("\nReranked:")
+    print("ndcg: " + str(ndcg_reranked))
+    precision_recall_f(query, hits_reranked)
