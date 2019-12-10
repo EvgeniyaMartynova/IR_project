422f0bb7c1911ef35277e8c0a5cdaa68aba96cd8
Evgenia Martynova <evg.martynova@gmail.com>
2019-12-11 00:38:53 +0100

    Affinity re-ranking improvements - eigen vector optimization, affinity ranking normalization, shifting of affinity scores to be > 0, plotting of scores of initial, affinity and final rankings. More detailed comments

--- src/re_ranking/affinity_ranking.py
+++ src/re_ranking/affinity_ranking.py
@@ -4,13 +4,19 @@ from sklearn.preprocessing import normalize
 import os
 import copy
 from nltk import download
+# nltk stopwords are used, because CountVectorizer's stop words list is controversal. Nltk's list is quite small
+# and thus should be more robust. Source https://www.aclweb.org/anthology/W18-2502
 from nltk.corpus import stopwords
+# scipy's implementation of eigen vector calculation is more robust
+# and applies different kinds of optimization for better numerical stability
+from scipy.linalg import eigh
 
+affinity_threshold = 0.1
 dumping_factor = 0.85
-download('stopwords')
+#download('stopwords')
 
 
-class Document:
+class InputDocument:
 
     def __init__(self, docid, content, query_similarity):
         self.docid = docid
@@ -41,6 +47,7 @@ def affinity(vector1, vector2):
 
 def convert_documents_to_vectors(collection):
     stop_words = stopwords.words('english')
+    # stop words are removed from vector space since they add no value to documents similarity
     count_vectorizer = CountVectorizer(stop_words=stop_words)
     document_vectors = count_vectorizer.fit_transform(collection)
     document_vectors = document_vectors.toarray()
@@ -59,26 +66,36 @@ def get_affinity_matrix(collection):
             if i != j:
                 affinity_matrix[i, j] = affinity(document_0, document_1)
 
-    return affinity_matrix
+    # normalize by division at max item to make matrix items belong to 0..1 interval
+    # but preserve the relation between all the matrix items
+    # optional, done for convenience of defining the threshold and its more intuitive understanding
+    return affinity_matrix /np.amax(affinity_matrix)
 
 
-def apply_threshold(affinity, affinity_threshold):
+def apply_threshold(affinity):
     return affinity if affinity >= affinity_threshold else 0
 
 
-# think about more efficient way to implement it and also how to pass "threshold" to apply_threshold as a parameter
+# TODO: for the final implementation get rid of this method and put the logic to get_affinity_matrix
+# Now it is still useful for debuging
 def get_adjacency_matrix(affinity_matrix):
     # the best criteria I came up with so far
-    median = np.median(affinity_matrix[np.where( affinity_matrix > 0)])
-    affinity_threshold = median * 0.25
+    median = np.median(affinity_matrix[np.where(affinity_matrix > 0)])
+    maximum = np.amax(affinity_matrix)
+    print("Affinity matrix maximum {}, median (excluding zero values) {}".format(maximum, median))
+    non_zero_items = np.size(affinity_matrix[np.where(affinity_matrix > affinity_threshold)])
+    size = np.shape(affinity_matrix)
+    print("Number of edges in affinity graph of {}. Graph number of nodes {}".format(non_zero_items, size[0]))
 
     # i think there is a better way to do it, but I haven't found it
-    size = np.shape(affinity_matrix)
     adjacency_matrix = np.zeros(size)
 
     for i in range(0, size[0]):
         for j in range(0, size[1]):
-            adjacency_matrix[i, j] = apply_threshold(affinity_matrix[i, j], affinity_threshold)
+            adjacency_matrix[i, j] = apply_threshold(affinity_matrix[i, j])
+
+    if np.count_nonzero(adjacency_matrix) == 0:
+        print("Warning: affinity graph has no edges")
 
     # we will use normalized matrix, this way of normalization will work if there are zero only rows
     return normalize(adjacency_matrix, axis=1, norm='l1')
@@ -91,23 +108,12 @@ def get_transition_matrix(adjacency_matrix, dumping_factor):
 
 
 def get_document_rank(transition_matrix):
-    eigen_values, eigen_vectors = np.linalg.eig(transition_matrix)
+    eigen_values, eigen_vectors = eigh(transition_matrix)
     # principal eigen vector is the one which corresponds to the largest eigen value
     principal_eigen_vector = eigen_vectors[:, eigen_values.argmax()]
     # normalize
     principal_eigen_vector /= principal_eigen_vector.sum()
     return principal_eigen_vector
-
-
-def get_documents():
-    docs = []
-    for the_file in os.listdir("docs"):
-        if the_file.endswith(".txt"):
-            file = open(os.path.join("docs", the_file), mode='r')
-            docs.append(file.read().strip())
-            file.close()
-
-    return docs
 
 
 def diversity_penalty(documents, adjacency_matrix):
@@ -118,16 +124,20 @@ def diversity_penalty(documents, adjacency_matrix):
         remaining_documents.sort(key=lambda x: x.score, reverse=True)
         top_document = remaining_documents.pop(0)
         diversity_penalized_ranking.append(top_document)
-        top_document_index = top_document.index
-        top_document_info_rich = top_document.info_rich
         for document in remaining_documents:
-            document_index = document.index
-            document.score = document.score - adjacency_matrix[document_index, top_document_index]*top_document_info_rich
+            document.score = document.score - adjacency_matrix[document.index, top_document.index]*top_document.info_rich
+
+    # shift scores to remove negative values so that we could apply log normalization
+    # shifting does not affect ranking and difference between scores
+    min_score = diversity_penalized_ranking[-1].score
+    if min_score < 0:
+        for document in diversity_penalized_ranking:
+            document.score -= min_score
 
     return diversity_penalized_ranking
 
 
-# documents is a list of Document class instances
+# documents is a list of InputDocument class instances
 def get_affinity_ranking(documents):
     documents_content = list(map(lambda x: x.content, documents))
     affinity_matrix = get_affinity_matrix(documents_content)
@@ -146,8 +156,20 @@ def get_affinity_ranking(documents):
     return diversity_penalized_ranking
 
 
+# TODO: mote to test folder
+def get_documents():
+    docs = []
+    for the_file in os.listdir("docs"):
+        if the_file.endswith(".txt"):
+            file = open(os.path.join("docs", the_file), mode='r')
+            docs.append(file.read().strip())
+            file.close()
+
+    return docs
+
+
 def main():
-    # TODO: mote to test folder
+    # TODO: mote to tests
     docs = get_documents()
 
     affinity_matrix = get_affinity_matrix(docs)
@@ -172,7 +194,7 @@ def main():
 
     diversity_penalized_ranking = diversity_penalty(ranked_documents, adjacency_matrix)
 
-    print("test")
+    print("stop")
 
 
 
--- src/re_ranking/re_ranking.py
+++ src/re_ranking/re_ranking.py
@@ -1,11 +1,14 @@
 
 from pyserini.search import pysearch
 import affinity_ranking as ar
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 # tunable coefficients for scores linear combination
-# alpha query similarity measure score
-# beta affinity ranking score
+# query similarity measure score
 alpha = 0.75
+# affinity ranking score
 beta = 1 - alpha
 
 
@@ -15,6 +18,31 @@ class ReRankedDocument:
         self.docid = docid
         self.content = content
         self.score = score
+
+
+def plot_scores(hits, ar_documents, re_ranked_documents):
+    max_similarity_score = hits[0].score
+    max_affinity_score = ar_documents[0].score
+
+    original_scores = list(map(lambda x: x.score / max_similarity_score, hits))
+    affinity_scores = list(map(lambda x: x.score / max_affinity_score, ar_documents))
+    re_ranked_scores = list(map(lambda x: x.score, re_ranked_documents))
+    t = np.arange(0, len(hits), 1)
+
+    plt.figure()
+    plt.subplot(311)
+    plt.plot(t, original_scores)
+    plt.suptitle('Original')
+
+    plt.subplot(312)
+    plt.plot(t, affinity_scores)
+    plt.suptitle('Affinity')
+
+    plt.subplot(313)
+    plt.plot(t, re_ranked_scores)
+    plt.suptitle('Re-ranked')
+    plt.show()
+
 
 '''
    results : list of io.anserini.search.SimpleSearcher$Result
@@ -30,29 +58,37 @@ def extract_docs_for_reranking(query, index_path, K):
 
 
 def re_rank_docs(hits):
-    docs_to_re_rank = list(map(lambda x: ar.Document(x.docid, x.content, x.score), hits))
-    af_ranking = ar.get_affinity_ranking(docs_to_re_rank)
+    docs_to_re_rank = list(map(lambda x: ar.InputDocument(x.docid, x.content, x.score), hits))
+    ar_documents = ar.get_affinity_ranking(docs_to_re_rank)
+
     max_similarity_score = hits[0].score
-    max_affinity_score = af_ranking[0].score
+    max_affinity_score = ar_documents[0].score
 
     re_ranked_documents = []
-    for document in af_ranking:
+    for document in ar_documents:
         normalized_similarity_score = document.query_similarity / max_similarity_score
-        # for some reason in paper they use log normalization, I do not see a reason for it yet and also with AR
-        # we get negative document score because of diversity penalty
+        # for some reason in paper they use average log normalization of affinity ranking.
+        # I tried to apply it after shifting the values to be > 0
+        # But it does make sense, because after normalization with log(max_affinity_score)
+        # the shape of the curve it the same as with average normalization
         normalized_affinity_score = document.score / max_affinity_score
         final_score = alpha*normalized_similarity_score + beta*normalized_affinity_score
         re_ranked_document = ReRankedDocument(document.docid, document.content, final_score)
         re_ranked_documents.append(re_ranked_document)
 
     re_ranked_documents.sort(key=lambda x: x.score, reverse=True)
+    # to compare the scores variation. I would include it into appendix to justify the normalization choice
+    # and also to point out how the shape of affinity ranks curve affects the results
+    # (this is the disadvantage in my opinion, see "images/Affinity ranking")
+    plot_scores(hits, ar_documents, re_ranked_documents)
+
     return re_ranked_documents
 
 
 def main():
     # Depends on local environment
     # original search results
-    hits = extract_docs_for_reranking("Masked Rider", "../../../data/index", 1000)
+    hits = extract_docs_for_reranking("Masked Rider", "../../../data/index", 100)
     # re-ranked search results
     re_ranked_docs = re_rank_docs(hits)
 
